35d78467955688e8ebc5cd238f1e58cfff222696
Jorge Rubio <246339851+jorchrbc@users.noreply.github.com>
2025-12-16 15:55:01 -0600

    Add Provider package, the Provider for registration and the User entity

---
+++ lib/presentation/providers/register_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:marketplace/domain/entities/user.dart';
+
+class RegisterProvider extends ChangeNotifier{
+
+  String name = '';
+  String phone = '';
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  User? user;
+
+  bool validateUser(){
+    return formKey.currentState!.validate();
+  }
+
+  void saveUser(){
+    if(validateUser()){
+      user = User(
+        name: name,
+        phone: phone,
+        email: email,
+        password: password
+      );
+      notifyListeners();
+    }
+  }
+}
